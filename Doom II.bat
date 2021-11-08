6298f0aa2d24b909e3b0a715ddfd1959a556ba91
Markus ⛅ <3259076+KAMiKAZOW@users.noreply.github.com>
2021-11-08 23:53:56 +0100

    Doom II
    Sync config file as well

--- Doom II.bat
+++ Doom II.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %ProgramFiles%\gzdoom
-gzdoom.exe -iwad "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 2\base\DOOM2.WAD" -savedir "%OneDrive%\Saved Games\GZDoom\Doom2"
+gzdoom.exe -iwad "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 2\base\DOOM2.WAD" -config "%OneDrive%\Saved Games\GZDoom\gzdoom.ini" -savedir "%OneDrive%\Saved Games\GZDoom\Doom2"
 exit
