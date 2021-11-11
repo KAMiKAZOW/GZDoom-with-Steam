7680859763155a93f70b63028580a581209633cc
Markus ⛅ <3259076+KAMiKAZOW@users.noreply.github.com>
2021-11-11 23:07:20 +0100

    Brutal Doom 64

---
+++ Brutal Doom 64.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %ProgramFiles%\gzdoom
+gzdoom.exe -iwad "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 2\base\DOOM2.WAD" -file "%ProgramFiles(x86)%\Brutal Doom 64\bd64mod_v2.5.pk3" "%ProgramFiles(x86)%\Brutal Doom 64\bd64maps_v2.5.pk3" "%ProgramFiles(x86)%\Brutal Doom 64\D64 Messages Freedoom.pk3" "%ProgramFiles(x86)%\Brutal Doom 64\music\DOOM 64 ULTIMATE MUSIC PACK.PK3" -config "%OneDrive%\Saved Games\GZDoom\gzdoom.ini" -savedir "%OneDrive%\Saved Games\GZDoom\BrutalDoom64"
+exit
