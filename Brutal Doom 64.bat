5d89ae5c38f0e3d1e6ee9a8777fde86f79035260
Markus ⛅ <3259076+KAMiKAZOW@users.noreply.github.com>
2022-09-17 12:08:48 +0200

    Brutal Doom 64

--- Brutal Doom 64.bat
+++ Brutal Doom 64.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %ProgramFiles%\gzdoom
-gzdoom.exe -iwad "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\freedoom2.wad" -file "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\bd64game_v2.5.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\bd64maps_v2.5.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\D64 Messages Freedoom.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\music\DOOM 64 ULTIMATE MUSIC PACK.PK3" -config "%OneDrive%\Saved Games\GZDoom\gzdoom.ini" -savedir "%OneDrive%\Saved Games\GZDoom\BrutalDoom64"
+gzdoom.exe -iwad "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\freedoom2.wad" -file "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\bd64game_v2.5.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\bd64maps_v2.5.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\Optional_Addons\D64_Messages_Freedoom.pk3" "%ProgramFiles(x86)%\Steam\steamapps\common\Doom 64\music\DOOM 64 ULTIMATE MUSIC PACK.PK3" -config "%OneDrive%\Saved Games\GZDoom\gzdoom.ini" -savedir "%OneDrive%\Saved Games\GZDoom\BrutalDoom64"
 exit
